862cd71959ae2dffa3304e41f81ff2bb65fd3b70
labderra <labderra@student.42malaga.com>
2025-07-13 16:42:20 +0200

    cpp03/ex00 done

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,41 +6,41 @@
 /*   By: labderra <labderra@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 19:40:56 by labderra          #+#    #+#             */
-/*   Updated: 2025/07/11 12:56:10 by labderra         ###   ########.fr       */
+/*   Updated: 2025/07/13 16:32:32 by labderra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap() : _name("default"), _health(10), _energy(10), _damage(0) {
-    std::cout << "Default constructor called" << std::endl;
+	std::cout << "Default constructor called" << std::endl;
 }
 
 ClapTrap::ClapTrap(const std::string& name) : _name(name), _health(10), _energy(10), _damage(0) {
-    std::cout << "Parametric constructor called" << std::endl;
+	std::cout << "Parametric constructor called" << std::endl;
 }
 
 ClapTrap::ClapTrap(const ClapTrap& c) : _name(c._name), _health(c._health), _energy(c._energy), _damage(c._damage) {
-    std::cout << "Copy constructor called" << std::endl;
+	std::cout << "Copy constructor called" << std::endl;
 }
 
 ClapTrap& ClapTrap::operator=(const ClapTrap& c) {
 	std::cout << "Copy assignment operator called" << std::endl;
-    if (this == &c)
-        return (*this);
+	if (this == &c)
+		return (*this);
 	this->_name = c._name;
 	this->_health = c._health;
 	this->_energy = c._energy;
 	this->_damage = c._damage;
-    return (*this);    
+	return (*this);    
 }
 
 ClapTrap::~ClapTrap() {
-	std::cout << "Destructor called" << std::endl;
+	std::cout << "Destructor called " << std::endl;
 }
     
-void    ClapTrap::attack(const std::string& target){
-    if (this->_energy && this->_health) {
+void	ClapTrap::attack(const std::string& target){
+	if (this->_energy && this->_health) {
 		std::cout << "ClapTrap " << this->_name << " attacks " << target << ", causing "<< this->_damage << " points of damage!" << std::endl;
 		this->_energy -= 1;
 	}
@@ -48,8 +48,8 @@ void    ClapTrap::attack(const std::string& target){
 		std::cout << "ClapTrap " << this->_name << " can not take any actions." << std::endl;
 }
 
-void    ClapTrap::takeDamage(unsigned int amount){
-    if (this->_health > amount) {
+void	ClapTrap::takeDamage(unsigned int amount){
+	if (this->_health > amount) {
 		std::cout << "ClapTrap " << this->_name << " suffers " << amount << " points of damage!" << std::endl;
 		this->_health -= amount;
 	}
@@ -61,8 +61,8 @@ void    ClapTrap::takeDamage(unsigned int amount){
 	std::cout << "ClapTrap " << this->_name << " was already destroyed!" << std::endl;
 }
 
-void    ClapTrap::beRepaired(unsigned int amount){
-    if (this->_energy) {
+void	ClapTrap::beRepaired(unsigned int amount){
+	if (this->_energy) {
 		std::cout << "ClapTrap " << this->_name << " recovers " << amount << " points of health!" << std::endl;
 		this->_health += amount;
 		this->_energy -= 1;
--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -3,10 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: labderra <labderra@student.42.fr>          +#+  +:+       +#+        */
+/*   By: labderra <labderra@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 19:40:22 by labderra          #+#    #+#             */
-/*   Updated: 2025/07/10 19:40:23 by labderra         ###   ########.fr       */
+/*   Updated: 2025/07/13 16:40:00 by labderra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+int	main() {
+	std::cout << "--- CREATE INSTANCES ---" << std::endl;
+	ClapTrap NoName;
+	ClapTrap Hollie = ClapTrap("Hollie");
+	
+	std::cout << "\n--- ATTACK ACTION ---" << std::endl;
+	Hollie.attack("Enemy");
+	Hollie.attack("Johnie");
+	
+	std::cout << "\n--- TAKES DAMAGE, STILL ALIVE ---" << std::endl;
+	Hollie.takeDamage(9);
+	Hollie.attack("Another Enemy");
+
+	std::cout << "\n--- TAKES DAMAGE, IS DESTROYED ---" << std::endl;
+	Hollie.takeDamage(1);
+	Hollie.attack("Last Enemy");
+
+	std::cout << "\n--- IS REPAIRED, USES ALL ENERGY ---" << std::endl;
+	Hollie.beRepaired(10);
+	Hollie.attack("Dummy");
+	Hollie.attack("Dummy");
+	Hollie.attack("Dummy");
+	Hollie.attack("Dummy");
+	Hollie.attack("Dummy");
+	Hollie.attack("Dummy");
+	Hollie.attack("Dummy");
+
+	std::cout << "\n--- MAKES COPY, STILL EXHAUSTED ---" << std::endl;
+	ClapTrap Johnie = ClapTrap(Hollie);
+	Johnie.attack("Dummy");
+	Johnie.~ClapTrap();
+	Johnie = Hollie;
+	Johnie.attack("Dummy");
+	
+	return(0);
+}
